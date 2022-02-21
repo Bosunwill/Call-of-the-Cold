5469ebd3d0c3c18afdd1077da70b2f9b219a6495
Gui Ndikum <54271663+Bosunwill@users.noreply.github.com>
2022-02-20 19:30:31 -0600

    Animator and Prefabs (#11)
    * Animator and Prefabs
    
    Added the prefabs character
    
    * New frames
    
    Changed 1 frame in V1 crouch. Added V2 idle + crouch.
    
    Co-authored-by: cjables <charles.joshua.bailey@gmail.com>
    Co-authored-by: Emorichii <78043575+Emorichii@users.noreply.github.com>

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class cameraController : MonoBehaviour
 {
     public Transform target;
 
