3734b50b034dd0ea70fcc5fdeb9889bdadadab8f
Gui Ndikum <54271663+Bosunwill@users.noreply.github.com>
2022-03-26 16:51:55 -0500

    UI Elements (#33)
    Pushing my UI changes hopefully they stick
    
    Co-authored-by: CJ_Bailey <charles.joshua.bailey@gmail.com>

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -30,28 +30,63 @@ public class playerController : MonoBehaviour
     // These sets of code control the hot and cold and players health
     //Collider triggers for Hot and Cold
 
+    public HealthBar healthBar;
+
     public float maxHealth = 100;
     [Range(0, 100)]
     public float currentHealth;
+    // Temps are in - because they need to rotate to the right
 
-    public float normalTemp = 32f;
-    [Range(32f, 100f)]
+    public float normalTemp = -45f;
+    [Range(0f, -90f)]
     public float currentTemp;
 
+    private bool isVicDead = false;
+
+    //Function on trigger to control temp on collision might be able to be 
+    //expanded for ambient temp system later
     void OnTriggerStay(Collider other)
     {
 
-        if (other.gameObject.tag == "Cold" && currentTemp >= 32f && currentHealth < 100)
+
+        if (other.gameObject.tag == "Cold" && currentTemp >= -90f)// && currentHealth < 100f)
         {
             currentTemp -= 3f * Time.deltaTime * 1;
 
             Debug.Log("You are cold" + currentHealth);
         }
-        if (other.gameObject.tag == "Hot" && currentTemp < 120f)
+        if (other.gameObject.tag == "Hot" && currentTemp <= -23f)
         {
+            Debug.Log("Hot");
             currentTemp += 1f * Time.deltaTime * 1;
+
             //    Debug.Log("You are hot" + currentHealth);
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other)
+        {
+            // Debug.Log("HIT");
+        }
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("You got hit");
+            currentHealth -= 200 * Time.deltaTime;
+         //   healthBar.SetHealth(currentHealth);
+
+        }
+    }
+
+
+    [SerializeField]
+    Transform tempArrow;
+
+    void TempGuage()
+    {
+        tempArrow.localRotation = Quaternion.Euler(0f, 0f, currentTemp);
+
     }
 
 
@@ -59,11 +94,14 @@ public class playerController : MonoBehaviour
     {
         currentHealth = maxHealth;
         currentTemp = normalTemp;
+      //  healthBar.SetMaxHealth(maxHealth);
     }
 
     void Update()
     {
 
+        TempGuage();
+        // Code for crouching and stoping movement when crouching
         if (Input.GetButtonDown("Crouch"))
         {
             Debug.Log("CROUCHING");
@@ -73,7 +111,7 @@ public class playerController : MonoBehaviour
         else if (Input.GetButtonUp("Crouch"))
         {
             isCrouched = false;
-            moveSpeed = 10;
+            moveSpeed = 4;
         }
 
         // not sure what this code does is this part of the inventory? -CJ
@@ -90,12 +128,16 @@ public class playerController : MonoBehaviour
         // Applies the inputs to the Rigidbody 
         theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
 
+        if (currentHealth < 0.1)
+        {
+            isVicDead = true;
+        }
+
         //Animator controller for movement recieved from the rigidbody
         anim.SetFloat("Speed", theRB.velocity.magnitude);
         anim.SetBool("isCrouched", isCrouched);
-
-
-        //Control for crouching 
+        anim.SetFloat("currentHealth", currentHealth);
+        anim.SetBool("victorDead", isVicDead);
 
 
         //Commented out to remove jumping but not deleted incase we change our minds
@@ -127,16 +169,20 @@ public class playerController : MonoBehaviour
         {
             theSR.flipX = false;
         }
-
-        if (currentTemp > 60f)
+        //temp code for temperature based healing. 
+        if (currentTemp > -23f)
         {
             Debug.Log("You are too hot!");
             currentHealth -= 1 * Time.deltaTime * 1;
+          //  healthBar.SetHealth(currentHealth);
+
         }
 
-        if (currentTemp < 40f && currentHealth < 100)
+        if (currentTemp < -70f && currentHealth < 100)
         {
             currentHealth += 1 * Time.deltaTime * 1;
+           // healthBar.SetHealth(currentHealth);
+
         }
 
     }
