2cc09d4132e857467823fbc0f86a8a81b77e1060
Gui Ndikum <54271663+Bosunwill@users.noreply.github.com>
2022-04-02 21:29:29 -0500

    Cassette Tape Done (#43)
    Co-authored-by: MageStep <77864237+MageStep@users.noreply.github.com>

---
+++ Assets/AudioLibrary.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLibrary : MonoBehaviour
+{
+    public AudioClip cas1;
+    public AudioClip cas2;
+    public AudioClip cas3;
+    public AudioSource audi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audi = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play1()
+    {
+        audi.PlayOneShot(cas1);
+    }
+    public void Play2()
+    {
+        audi.PlayOneShot(cas2);
+    }
+    public void Play3()
+    {
+        audi.PlayOneShot(cas3);
+    }
+}
