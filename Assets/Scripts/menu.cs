28c70ae2ca9142958e5fff96f36765b7e139aa7c
Gui Ndikum <54271663+Bosunwill@users.noreply.github.com>
2022-04-07 08:50:41 -0500

    A lot done on menu and level 1 (#47)
    - Splash screen text animate
    -Pause Menu text increase and exit button taken off
    - Inventory cabinet taken off level 1
    - Game can be plaid from Main menu up to player moving from level1 scene 2, to level 2 scene 3

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -4,10 +4,49 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
-{
+{   
+    bool enter = false;
+    List<string> Einventory = new List<string>{"isCassette1", "isCassette1", "isCassette2"};
     public void GamePlay ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    void LateUpdate()
+    {
+        Content();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (enter == true)
+        {
+             SceneManager.LoadScene(2);
+        }
+       
+    }
+
+    void Content()
+    {
+        Debug.Log("Content is called");
+        for (int j = 0; j < Einventory.Count; ++j) 
+        {
+            Debug.Log("I search Inventory");
+            if (Einventory[j] == "isCassette1")
+            {
+                ++j;
+                if (j == 1)
+                {
+                    enter = true;
+                    this.GetComponent<Collider>().isTrigger = true;
+                    Debug.Log("I have an Item");
+                }
+                else
+                {
+                    enter = false;
+                }
+            }
+
+        }
     }
 
     public void EndGame ()
